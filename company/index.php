843e4e36f66cb599460453f353105f69e485c02a
Criesha Ann Borbajo-Barcoma <borbajoc@gmail.com>
2022-11-03 10:52:13 -0400

    few changes

--- company/index.php
+++ company/index.php
@@ -134,7 +134,7 @@
 		<?php include_once('../lib/sidebar_menu.php'); ?>
 		<div class="container-fluid">
       		<div class="row">
-			  	<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+			  	<div class="col-sm-6 col-sm-offset-3 col-md-8 col-md-offset-2 main">
 				  
 
 					<div class="row placeholders">
